c32f4c314ff9b61f77a664ca7aa61403b6728239
kevinhou111@gmail.com <kevinhou111@gmail.com>
2023-12-22 12:34:11 +1300

    v1.0 export gcal and import notion base function

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# ncal
-Google Calendar to Notion sync
+<h1 align="center"> NCal </h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/vectara/vectara-ingest#readme" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-no-red.svg" />
+  </a>
+  <a href="https://github.com/vectara/vectara-ingest/graphs/commit-activity" target="_blank">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-no-red.svg" />
+  </a>
+  <a href="https://twitter.com/kevinychou" target="_blank">
+    <img alt="Twitter: kevinychou" src="https://img.shields.io/twitter/follow/kevinychou.svg?style=social" />
+  </a>
+</p>
+
+`Google Calendar to Notion sync`
+
+<p align="center">
+  <img style="max-width: 100%;" alt="notion image" src="img/notion_image.png"/>
+</p>
+
+# About this project
+If you're like me, you're obssessed with two things - Google Calendar and Notion. However, there isn't an easy way to visualise the time you spend on various things - hence the goal of this project is to sync this information into a Notion database, where you can more easily see trends in your data over time.
+
+*Aside*: I believe future knowledge management systems will find better ways to integrate the calendar with all our other productivity tools.
+
+This project had two goals:
+- Create a useful app that is useful for tracking, personal reflections, allowing me to track how my time is being used week-to-week
+- Get practice using Notion/GCal API to build something + a fun personal coding project to keep me up to scratch with Python.
+
+For more information on this project, please see: https://kevinychou.notion.site/Project-NCal-4808de8d50c8471ab225f22ff96f6535?pvs=4
+
+## Setup
+To run this repo, you need a bit of setup: See these helpful links
+- Google Calendar API Project: https://developers.google.com/calendar/api/quickstart/python
+- Notion API: https://developers.notion.com/docs/create-a-notion-integration
+
+## Usage
+There are three input .json files you need in order to run the script. I've included examples of each in the git repo under /json_examples
+- calendar.json: This holds the specific colour mappings you have, and the requisite column name in Notion.
+- credentials.json: This is the Google API credentials given to you via creating a Google Cloud app.
+- variables.json: This is the list of calendars you want to find events from, the Notion database ID you want to pull from, and your created Notion secret from the Notion API.
+
+```
+python main.py
+```
+
+## Upcoming Features
+[] Implement CRON job to automate running this script at the end of every week
+[] Add options for more tracking via event name as opposed to just event colour
+[] Take "description" from Google Calendar events, and use this metadata to populate other databases (i.e. I also track my gym workouts using GCal).
---
+++ main.py
@@ -0,0 +1,213 @@
+from datetime import datetime, timedelta
+import os.path
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from notion_client import Client
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+with open("variables.json") as f:
+    variables = json.load(f)
+    ACCEPTED_CALENDARS = variables["calendars"]
+    DATABASE_ID = variables["database_id"]
+    NOTION_SECRET = variables["notion_secret"]
+
+# Authenticate with Google Calendar API
+def authenticate_google():
+    credentials = None
+    token_path = 'token.json'
+
+    if os.path.exists(token_path):
+        credentials = Credentials.from_authorized_user_file(token_path, SCOPES)
+
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES
+            )
+            credentials = flow.run_local_server(port=0)
+        with open(token_path, 'w') as token:
+            token.write(credentials.to_json())
+
+    return build('calendar', 'v3', credentials=credentials)
+
+# Fetch Google Calendar Events
+def fetch_google_events(service):
+    color_mapping_path = "calendar.json"
+
+    # Load color mapping from JSON file
+    with open(color_mapping_path, 'r') as json_file:
+        color_mapping = json.load(json_file)
+
+    # Get the calendar list
+    calendars = service.calendarList().list().execute().get('items', [])
+
+    # Define the start and end dates for the recent week
+    end_date = datetime.utcnow()
+    start_date = end_date - timedelta(days=7)
+
+    # Fetch events from each calendar within the recent week
+    all_events = []
+    for calendar in calendars:
+        calendar_id = calendar['id']
+        if calendar_id not in ACCEPTED_CALENDARS:
+            continue
+
+        events = service.events().list(
+            calendarId=calendar_id,
+            timeMin=start_date.isoformat() + 'Z',
+            timeMax=end_date.isoformat() + 'Z'
+        ).execute().get('items', [])
+
+        for event in events:
+            if event['status'] == "cancelled":
+                continue
+            # Extract relevant information
+            start_time = event['start'].get('dateTime', event['start'].get('date'))
+            end_time = event['end'].get('dateTime', event['end'].get('date'))
+            summary = event.get('summary', 'No Title')
+            color_id = event.get('colorId', None)
+            description = event.get('description', None)
+
+            # Map color to item using the provided JSON file
+            item = color_mapping.get(color_id, 'Uncategorized')
+
+            # Convert time strings to datetime objects
+            start_datetime = datetime.fromisoformat(start_time)
+            end_datetime = datetime.fromisoformat(end_time)
+
+            time_difference_seconds = (end_datetime - start_datetime).total_seconds()
+            time = round(time_difference_seconds / 3600, 2)
+
+            # Checks that not ongoing event
+            if time < 24:
+                event_details = {
+                    'summary': summary,
+                    'item': item,
+                    'start_datetime': start_datetime,
+                    'end_datetime': end_datetime,
+                    'hours': time,
+                    'color_id': color_id,
+                    'description': description 
+                }
+                all_events.append(event_details)
+
+    return all_events
+
+# Authenticate with Notion API
+def authenticate_notion():
+    # Use the Notion integration token for authentication
+    return Client(auth=NOTION_SECRET)
+
+# Insert Events into Notion Database
+def insert_into_notion(client, events, calendar_mapping):        
+    # Get last entry number
+    temp_db = client.databases.query(
+        **{
+	        "database_id": DATABASE_ID,
+	        "sorts": [
+                {
+                "property": "Entry Number",
+                "direction": "descending"
+                }
+            ],
+    	})
+    last_entry_no = temp_db["results"][0]["properties"]["Entry Number"]["title"][0]["plain_text"]
+
+    # Convert calendar information
+    time_values = init_time_values(calendar_mapping)
+    for event in events:
+        color_id = event['color_id']
+        try:
+            type = calendar_mapping[color_id].get("type")
+        except:
+            Exception("Colours not mapped correctly.")
+        time = event["hours"]
+        time_values[type]["number"] += time
+    
+    # Set new values
+    new_entry_dict = {}
+    entry_dict = generate_entry_dict(int(last_entry_no))
+    date_dict = generate_date_dict()
+    new_entry_dict.update({"Entry Number": entry_dict})
+    new_entry_dict.update({"Date": date_dict})
+    new_entry_dict.update(time_values)
+
+    # Import properties into new row
+    print(new_entry_dict)
+    client.pages.create(
+        **{
+            "parent": {"database_id": DATABASE_ID},
+            "properties": new_entry_dict
+        })
+    return
+
+def init_time_values(calendar_mapping):
+    output_dict = {}
+    for item in calendar_mapping.values():
+        new_entry = {
+            "type": "number",
+            "number": 0
+        }
+        output_dict.update({item['type']: new_entry})
+
+    return output_dict
+
+def generate_date_dict():
+    output_dict = {
+        "type": "date",
+        "date": {
+            "start": datetime.today().strftime('%Y-%m-%d'),
+            "end": None,
+            "time_zone": None
+        }
+    }
+    return output_dict
+
+def generate_entry_dict(last_entry_no):
+    output_dict = {
+        "id": "title",
+        "type": "title",
+        "title": [
+            {
+                "type": "text",
+                "text": {
+                    "content": str(last_entry_no+1),
+                    "link": None
+                },
+                "annotations": {
+                    "bold": False,
+                    "italic": False,
+                    "strikethrough": False,
+                    "underline": False,
+                    "code": False,
+                    "color": "default"
+                },
+                "plain_text": str(last_entry_no+1),
+                "href": None
+            }
+        ]
+    }
+    return output_dict
+
+# Main Script
+if __name__ == '__main__':
+    google_service = authenticate_google()
+    notion_client = authenticate_notion()
+
+    # Fetch events from Google Calendar
+    events = fetch_google_events(google_service)
+
+    # Insert events into Notion
+    with open("calendar.json") as f:
+        calendar_mapping = json.load(f)
+
+    insert_into_notion(notion_client, events, calendar_mapping)
+    
